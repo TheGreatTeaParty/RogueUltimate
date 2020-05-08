d454923e0ff9693868de760f7f1a6ae25cd30983
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2020-05-08 20:58:26 +0300

    Update PlayerMovment.cs
    There is description for PlayerMovement script :)

--- Assets/Scripts/Player/PlayerMovment.cs
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour {
+
+    /*I've not understood exactly yet, but dudes with the big dicks in the Internet 
+    said that it's better to use [SerializeField]*/ 
     [SerializeField] private float movement_speed = 10.0f;
     private Rigidbody2D rb2D;
     private Vector2 movement;
@@ -11,18 +14,22 @@ public class PlayerMovment : MonoBehaviour {
         rb2D = this.GetComponent<Rigidbody2D>();
     }
 
+    /*There we receive input information (WASD or arrows or mobile stick)*/
     void Update() {
         movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
 
+    /*I called func. moveCharacter there, because FixedUpdate is better for physic detection*/
     void FixedUpdate() {
         moveCharacter(movement);
     }
     
+    /*This is the main function which calculate Character's movement*/
     void moveCharacter(Vector2 movement_direction){
-        rb2D.MovePosition((Vector2)transform.position + (movement_speed * 
-                                                     movement_direction * 
-                                                        Time.deltaTime));
+        rb2D.MovePosition((Vector2)transform.position + 
+                                    (movement_speed * 
+                                    movement_direction * 
+                                        Time.deltaTime));
     }
 
 }
