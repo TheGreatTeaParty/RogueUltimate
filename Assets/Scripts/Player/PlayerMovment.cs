9331185e9c1d51648d7922409b1d2c9bc9222b6e
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2020-05-08 20:31:36 +0300

    Movement script
    I've rewrote script.
    Basicly, it's working

--- Assets/Scripts/Player/PlayerMovment.cs
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -2,44 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovment : MonoBehaviour
-{
-    public float move_speed;
+public class PlayerMovment : MonoBehaviour {
+    [SerializeField] private float movement_speed = 10.0f;
+    private Rigidbody2D rb2D;
+    private Vector2 movement;
 
-    private Vector3 movement;
-    private Rigidbody2D body;
-
-    //Add Rigidbody for the future, there is no need in it right now
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>(); 
+    private void Start() {
+        rb2D = this.GetComponent<Rigidbody2D>();
     }
 
-   
-    void Update()
-    {
-        //Make direction 0,0,0 if user do not input
-        movement = new Vector3(0f, 0f, 0f);
-
-        //Checking for user input and change direction;
-        if (Input.GetKey(KeyCode.A))
-        {
-            movement = new Vector3(-1f, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            movement = new Vector3(1f, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            movement = new Vector3(0f, 1f, 0);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            movement = new Vector3(0f, -1f, 0);
-        }
-
-        //Tranform the position of the player
-        transform.position += movement * move_speed * Time.deltaTime;
+    void Update() {
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
+
+    void FixedUpdate() {
+        moveCharacter(movement);
+    }
+    
+    void moveCharacter(Vector2 movement_direction){
+        rb2D.MovePosition((Vector2)transform.position + (movement_speed * 
+                                                     movement_direction * 
+                                                        Time.deltaTime));
+    }
+
 }
