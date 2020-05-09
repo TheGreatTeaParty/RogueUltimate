6fca57273fbf835223385192bfe5703a6fa2f088
batovski <56854166+batovski@users.noreply.github.com>
2020-05-09 10:18:38 -0700

    DoNotDestroy
    DO NOT DESTROY!!!

---
+++ Assets/Scripts/MapGeneration/InterfaceOnScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceOnScene : MonoBehaviour
+{
+    public static InterfaceOnScene instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
