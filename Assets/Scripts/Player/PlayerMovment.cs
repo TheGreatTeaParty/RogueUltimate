c80c008c94dbc5888d7c9eaf4a8c136f80cd2346
puzz13 <puzz13one@gmail.com>
2020-05-27 00:16:01 +0300

    Inventory remake

--- Assets/Scripts/Player/PlayerMovment.cs
+++ Assets/Scripts/Player/PlayerMovment.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovment : MonoBehaviour 
 {
- 
     [SerializeField] private float movement_speed = 10.0f;
     [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private Animator animator; 
     [SerializeField] protected Joystick joystick;
     private Vector2 movement;
 
+
+    public void Start()
+    {
+        joystick = InterfaceOnScene.instance.GetComponentInChildren<Joystick>();
+    }
 
     /*There we receive input information*/
     void Update() 
@@ -23,12 +28,14 @@ public class PlayerMovment : MonoBehaviour
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
     }
+    
 
     /*I called func. moveCharacter there, because FixedUpdate is better for physic detection*/
     void FixedUpdate() 
     {
         moveCharacter(movement);
     }
+    
     
     /*This is the main function which calculate Character's movement*/
     void moveCharacter(Vector2 movement_direction)
@@ -39,6 +46,7 @@ public class PlayerMovment : MonoBehaviour
                                         Time.deltaTime));
     }
 
+    
     public void attackCharacter(){
         animator.SetTrigger("Attack");
 
