69ad0250213d898f9a4a533cd02dd695dd669200
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2020-07-16 23:23:00 +0300

    Some Changes
     - Added 2 files for future sva system
     - Added animation of taking dmg
     - Added "beta" settings menu in pause menu
     - Added logic of death
    
    - Changed position of bars from top-left corner to the down-middle

---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    public int HP;
+    public int MP;
+    public int SP;
+    public float[] position;
+
+    public PlayerData(PlayerStat player_stat)
+    {
+        HP = PlayerStat.Instance.currentHealth;
+        MP = PlayerStat.Instance.currentMana;
+        SP = PlayerStat.Instance.currentStamina;
+
+        position = new float[3];
+        position[0] = PlayerStat.Instance.transform.position.x;
+        position[1] = PlayerStat.Instance.transform.position.y;
+        position[2] = PlayerStat.Instance.transform.position.z;
+    }
+
+}
