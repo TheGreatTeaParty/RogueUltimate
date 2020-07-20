8742a0ab322fbed8c38b7bbee24bf46424acd6a2
batovski <56854166+batovski@users.noreply.github.com>
2020-07-20 03:45:01 -0700

    AI Rework#1
    -Added new AI system
    -Archer, Warioir, Kaban, Boss reworked
    -Optimization of the pathfinding process
    -More flexibility in imolementation

---
+++ Assets/Scripts/NPC/NPC/NPCenemy/Boss/GolemBoss.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolemBoss : AI
+{
+    public float speed = 6f;
+    public float meleRange = 0.5f;
+    public float rockRange = 7f;
+    public float detectionRange = 7f;
+    public float followRange = 10f;
+
+    [Space]
+    public int StageTwoHealth = 180;
+    public float StageCharacteristicMult = 1.4f;
+
+    [Space]
+    public float KnockBack = 1f;
+    public Transform rockPrefab;
+
+    private BossStage stage;
+    private EnemyStat golemStat;
+
+   // private NPCPathfindingMovement NPCmovement;
+    private bool StageChanged = false;
+
+
+    public enum BossStage
+    {
+        first = 0,
+        second,
+    };
+    
+    public override void Start()
+    {
+       // NPCmovement = GetComponent<NPCPathfindingMovement>();
+        //NPCmovement.SetSpeed(speed);
+        golemStat = GetComponent<EnemyStat>();
+        stage = BossStage.first;
+
+        target = GameObject.FindGameObjectWithTag("Player");
+        startAttackCoolDown = attackCoolDown;
+        BossFightPortal.Instance.HealthBar(true);
+    }
+
+    public override void Update()
+    {
+        if (startAttackCoolDown > 0)
+            startAttackCoolDown -= Time.deltaTime;
+
+        if (golemStat.currentHealth <= StageTwoHealth && !StageChanged)
+        {
+            SwitchStage();
+        }
+        //Should be changed in the inherited EnemyStat class
+        BossFightPortal.Instance.SetBossHealth(golemStat.currentHealth);
+
+        if(golemStat.currentHealth <=0)
+        {
+            BossFightPortal.Instance.HealthBar(false);
+            BossFightPortal.Instance.TurnThePortal();
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        if (stage == BossStage.first)
+        {
+            switch (state)
+            {
+                case NPCstate.chasing:
+                    {
+                        StateChasing();
+                        break;
+                    }
+
+                case NPCstate.attacking:
+                    {
+                        StateAttack();
+                        break;
+                    }
+            }
+        }
+        else
+        {
+            switch (state)
+            {
+                case NPCstate.chasing:
+                    {
+                        StateChasing();
+                        break;
+                    }
+
+                case NPCstate.attacking:
+                    {
+                        StateAttack();
+                        break;
+                    }
+            }
+        }
+    }
+
+    
+    private void SwitchStage()
+    {
+        stage = BossStage.second;
+        speed *= StageCharacteristicMult;
+        attackCoolDown -= .2f;
+        StageChanged = true;
+    }
+
+    public override void Attack()
+    {
+        if (Vector2.Distance(transform.position, target.transform.position) < rockRange &&
+            Vector2.Distance(transform.position, target.transform.position) < meleRange)
+            MeleAttack();
+
+        else
+        {
+            RangeAttack();
+        }
+    }
+
+    private void MeleAttack()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        Vector3 attackPosition = transform.position + direction;
+
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition, meleRange, LayerMask.GetMask("Player"));
+        for (int i = 0; i < enemiesToDamage.Length; i++)
+        {
+     
+            enemiesToDamage[i].GetComponent<IDamaged>().TakeDamage(golemStat.physicalDamage.GetValue(), golemStat.magicDamage.GetValue());
+        }
+    }
+    private void RangeAttack()
+    {
+        Transform rock = Instantiate(rockPrefab, transform.position + (target.transform.position - transform.position).normalized*2, Quaternion.identity);
+
+        rock.GetComponent<FlyingObject>().SetData(golemStat.physicalDamage.GetValue(), golemStat.magicDamage.GetValue(), (target.transform.position - transform.position).normalized);
+    }
+}
---
+++ Assets/Scripts/NPC/NPC/NPCenemy/Warior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warior : AI
+{
+    public float AttachRange;
+
+    private LayerMask whatIsEnemy;
+    private Vector2 attackPosition;
+    private Vector3 direction;
+
+    private EnemyStat WariorStat;
+
+    public override void Start()
+    {
+        base.Start();
+        WariorStat = GetComponent<EnemyStat>();
+        whatIsEnemy = LayerMask.GetMask("Player");
+    }
+
+    public override void Update()
+    {
+        base.Update();
+       // direction = NPCmovement.GetLastMoveDir();
+        attackPosition = transform.position + direction;
+    }
+
+    public override void Attack()
+    {
+            Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition, AttachRange, whatIsEnemy);
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                enemiesToDamage[i].GetComponent<IDamaged>().TakeDamage(WariorStat.physicalDamage.GetValue(), WariorStat.magicDamage.GetValue());
+            }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPosition, AttachRange);
+    }
+}
