8d8ad6f13adba660ef8f9e8f13d1c17babc5a010
batovski <56854166+batovski@users.noreply.github.com>
2020-08-14 23:47:58 -0700

    Dynamic Display System

---
+++ Assets/Scripts/Renderer/DynamicLayerRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicLayerRenderer : MonoBehaviour
+{
+    [SerializeField]
+    private int baseLayer = 5000;
+
+    [SerializeField]
+    private float offSet = 0f;
+
+    [SerializeField]
+    private bool runOnlyOnce = false;
+
+    private Renderer myRenderer;
+
+    
+    void Start()
+    {
+        myRenderer = GetComponent<Renderer>();
+        InvokeRepeating("RenderObject", 0.1f, 0.3f);
+    }
+
+    private void RenderObject()
+    {
+        myRenderer.sortingOrder = (int)(baseLayer - transform.position.y - offSet);
+        if (runOnlyOnce)
+            Destroy(this);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(new Vector3(transform.position.x, transform.position.y - offSet, transform.position.z),0.05f);
+    }
+}
