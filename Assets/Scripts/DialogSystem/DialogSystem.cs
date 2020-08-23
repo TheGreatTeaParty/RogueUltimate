826416c3ede073b686f821746ef2bbd82fbad8f0
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2020-08-23 14:11:34 +0300

    Dialog system
     - Added dialog system
     - Made npc script citizen
     - Added to a Hub Elisa
     - Changed font in MainMenu

---
+++ Assets/Scripts/DialogSystem/DialogSystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DialogSystem : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI nameText;
+    [SerializeField]
+    private TextMeshProUGUI dialogText;
+    [SerializeField]
+    private GameObject dialogWindow;
+    private Queue<string> sentences;
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialog dialog)
+    {
+        Debug.Log("Talk to " + dialog.name);
+        nameText.text = dialog.name;
+
+        sentences.Clear();
+
+        foreach(string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDiolog();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(WriteSentence(sentence));
+    }
+
+    public void EndDiolog()
+    {
+        dialogWindow.gameObject.SetActive(false);
+        InterfaceOnScene.instance.gameObject.SetActive(true);
+        Debug.Log("End of diolog ");
+    }
+
+    IEnumerator WriteSentence (string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/NPC/NPC/NPCfiendly/Citizen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Citizen : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    private DialogSystem dialogSystem;
+    [SerializeField]
+    private GameObject dialogWindow;
+    [SerializeField]
+    private GameObject buttonContinue;
+    [SerializeField]
+    private Dialog dialog;
+    
+
+    void IInteractable.Interact()
+    {
+        InterfaceOnScene.instance.gameObject.SetActive(false);
+        dialogWindow.SetActive(true);
+        buttonContinue.SetActive(true);
+        dialogSystem.StartDialog(dialog);
+
+    }
+
+    string IInteractable.GetActionName() 
+    {
+        return "Talk";
+    }
+}   
