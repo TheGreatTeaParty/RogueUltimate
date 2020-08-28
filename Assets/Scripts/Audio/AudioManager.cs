b974c880e715eaa2f3abd693434f18e4182059df
puzz13 <puzz13one@gmail.com>
2020-08-28 15:56:28 +0300

    Quick menu remake + audiosystem base

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+
+namespace Audio
+{
+
+    
+    public class AudioManager : MonoBehaviour
+    {
+        public Sound[] sounds;
+
+
+        private void Awake()
+        {
+            foreach (Sound s in sounds)
+            {
+                
+            }
+        }
+    }
+
+
+}
