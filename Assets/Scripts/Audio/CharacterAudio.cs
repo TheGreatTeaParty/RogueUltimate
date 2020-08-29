90db8930ad1fb124c44f07d280ee60bf3a9136af
batovski <56854166+batovski@users.noreply.github.com>
2020-08-29 03:04:41 -0700

    Player Sounds, Chest sound

---
+++ Assets/Scripts/Audio/CharacterAudio.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAudio : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip[] step_sounds;
+
+    [SerializeField]
+    AudioClip take_damage;
+
+    [SerializeField]
+    AudioClip death;
+
+    [SerializeField]
+    AudioClip[] attack;
+
+    [SerializeField]
+    AudioClip swing;
+
+    [SerializeField]
+    AudioSource[] source;
+    public enum Type
+    {
+        fist,
+        mele,
+        bow,
+        magic,
+    };
+    
+    public void StepSound()
+    {
+        source[0].PlayOneShot(step_sounds[Random.Range(0, step_sounds.Length)]);
+    }
+    public void DamageSound()
+    {
+        
+        source[1].PlayOneShot(take_damage);
+        
+    }
+    public void DeathSound()
+    {
+       
+        source[1].PlayOneShot(death);
+        
+    }
+    public void AttackSound(Type type)
+    {
+        
+        switch (type)
+        {
+            case Type.fist:
+                {
+                    source[1].PlayOneShot(attack[0]);
+                    break;
+                }
+            case Type.mele:
+                {
+                    source[1].PlayOneShot(attack[1]);
+                    break;
+                }
+        }
+        
+    }
+    public void SwingSound()
+    {
+        source[1].PlayOneShot(swing);
+    }
+}
---
+++ Assets/Scripts/Audio/ChestAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAudio : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip open_sound;
+
+    [SerializeField]
+    AudioClip money_sound;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    public void OpenSound()
+    {
+        source.PlayOneShot(open_sound);
+    }
+    public void MoneySound()
+    {
+        source.PlayOneShot(money_sound);
+    }
+}
