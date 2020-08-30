32dc238749c175b39129598b2071e981041384ea
batovski <56854166+batovski@users.noreply.github.com>
2020-08-29 22:45:49 -0700

    XP bar, Portal audio

---
+++ Assets/Scripts/Audio/AudioRange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRange : MonoBehaviour
+{
+    private AudioSource source;
+
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            source.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            source.Stop();
+        }
+    }
+}
