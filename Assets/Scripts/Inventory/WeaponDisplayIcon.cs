0efef6b03451e74b5b967cb950d6a63201b09772
batovski <56854166+batovski@users.noreply.github.com>
2020-09-05 03:31:17 -0700

    WeaponDisplayIcon

---
+++ Assets/Scripts/Inventory/WeaponDisplayIcon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponDisplayIcon : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite BaseIcon;
+    [SerializeField]
+    private Image Icon;
+
+    private EquipmentManager equipmentManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        equipmentManager = EquipmentManager.Instance;
+        //Make delegate function from InventoryManager be equal to UpdateUI fun
+        equipmentManager.onEquipmentChanged += UpdateIcon;
+    }
+
+    // Update is called once per frame
+    void UpdateIcon(EquipmentItem newItem, EquipmentItem oldItem)
+    {
+        if(newItem!= null && newItem.equipmentType == EquipmentType.Weapon)
+        {
+            Icon.sprite = newItem.Sprite;
+        }
+        else
+        {
+            Icon.sprite = BaseIcon;
+        }
+    }
+}
