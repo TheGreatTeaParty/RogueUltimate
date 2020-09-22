60fb102ecfdbef2d4b9c1961d2e1858bd326d6ac
puzz13 <puzz13one@gmail.com>
2020-09-22 17:57:00 +0300

    Uh

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+
+    private void Start()
+    {
+        transform.position = target.position + offset;
+    }
+
+    private void Update()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+    
+}
