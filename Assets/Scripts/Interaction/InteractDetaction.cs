53cb59e534542a91790804316b8afd9d6729bbb5
batovski <56854166+batovski@users.noreply.github.com>
2020-12-01 17:01:50 -0800

    Outline, Dungeon darkness effect, Dynamic minimap

--- Assets/Scripts/Interaction/InteractDetaction.cs
+++ Assets/Scripts/Interaction/InteractDetaction.cs
@@ -4,19 +4,31 @@ public class InteractDetaction : MonoBehaviour
 {
     private InteractionUI interactionUI;
     private IInteractable interactable;
+    private MaterialPropertyBlock _collideMaterial;
+    private SpriteRenderer _colliderInfo;
 
     
     private void Start()
     {
         interactionUI = InterfaceManager.Instance.GetComponentInChildren<InteractionUI>();
+        _collideMaterial = new MaterialPropertyBlock();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<IInteractable>() != null)
         {
+            _colliderInfo = collision.GetComponent<SpriteRenderer>();
+
+            //Save colide material
+            _colliderInfo.GetPropertyBlock(_collideMaterial);
+
+
             //Save collission information to use it later in Call
             interactable = collision.GetComponent<IInteractable>();
+
+            //turn on the outline
+            TurnOnOutline();
 
             //Enable
             interactionUI.SetActive(true);
@@ -31,11 +43,14 @@ public class InteractDetaction : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.GetComponent<IInteractable>() != null)
+        {
             DeleteInteractionData();
+        }
     }
 
     public void DeleteInteractionData()
     {
+        TurnOFFOutline();
         interactionUI.SetActive(false);
         interactable = null;
     }
@@ -44,5 +59,21 @@ public class InteractDetaction : MonoBehaviour
     {
         interactable.Interact();
     }
-    
+
+    private void TurnOnOutline() 
+    {
+        _collideMaterial.SetFloat("_Thickness", 0.01f);
+        _colliderInfo.SetPropertyBlock(_collideMaterial);
+    }
+
+    private void TurnOFFOutline()
+    {
+        if (_colliderInfo)
+        {
+            _collideMaterial.SetFloat("_Thickness", 0.0f);
+            _colliderInfo.SetPropertyBlock(_collideMaterial);
+            _colliderInfo = null;
+        }
+    }
+
 }
