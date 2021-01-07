1d0a351b97047b4bcd95cc96c38f907db11259c5
Vladimir Mishakov <vladimir-mishakov@outlook.com>
2021-01-07 15:29:56 +0300

    Add better dialogue system.

---
+++ Assets/Scripts/DialogSystem/CutsceneDialogue/CutsceneDialogueManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneDialogueManager : MonoBehaviour, IDialogueManager
+{
+    [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private Image image;
+
+    [SerializeField] private Canvas UI;
+
+    // Desperate times call for desperate measures.
+    // Arrays that store the local position of DialogueUI elements.
+    [SerializeField] private Vector2[] leftLayoutPositions;
+    [SerializeField] private Vector2[] rightLayoutPositions;
+
+    private int _lineIndex = 0;
+    private Dialogue _currentDialogue;
+
+    /// <summary>
+    /// Hide UI and show dialogue window.
+    /// </summary>
+    /// <param name="dialogue">Dialogue that need to be shown.</param>
+    public void StartDialogue(Dialogue dialogue)
+    {
+        UI.enabled = false;
+        _currentDialogue = dialogue;
+        ChangeDialogueUIState(true);
+        NextLine();
+    }
+
+    public void NextLine()
+    {
+        if (_lineIndex >= _currentDialogue.Lines.Length)
+        {
+            ChangeDialogueUIState(false);
+            UI.enabled = true;
+        }
+        
+        if (_lineIndex < _currentDialogue.Lines.Length)
+        {
+            characterName.text = _currentDialogue.Lines[_lineIndex].CharacterName;
+            text.text = _currentDialogue.Lines[_lineIndex].DialogueLine;
+            image.sprite = _currentDialogue.Lines[_lineIndex].Icon;
+            ChangeLayout(_currentDialogue.Lines[_lineIndex].IconPosition);
+            _lineIndex++;
+        }
+    }
+
+    public void ChangeDialogueUIState(bool state)
+    {
+        characterName.enabled = state;
+        text.enabled = state;
+        image.enabled = state;
+    }
+
+    public void ChangeLayout(Line.Position position)
+    {
+        switch (position)
+        {
+                case Line.Position.Left:
+                    characterName.rectTransform.localPosition = leftLayoutPositions[0];
+                    text.rectTransform.localPosition = leftLayoutPositions[1];
+                    text.alignment = TextAlignmentOptions.Left;
+                    image.rectTransform.localPosition = leftLayoutPositions[2];
+                    break;
+                
+                case Line.Position.Right:
+                    characterName.rectTransform.localPosition = rightLayoutPositions[0];
+                    text.rectTransform.localPosition = rightLayoutPositions[1];
+                    text.alignment = TextAlignmentOptions.Right;
+                    image.rectTransform.localPosition = rightLayoutPositions[2];
+                    break;
+        }
+    }
+}
--- Assets/Scripts/Tutorial/TutorialManager.cs
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TutorialManager : MonoBehaviour
 {
-    [SerializeField] private DialogueUI dialogueUI;
+    [SerializeField] private CutsceneDialogueManager cutsceneDialogueManager;
 
     private void Start()
     {
+        StartDialogue();
+    }
+
+    private void StartDialogue()
+    {
         var dialogue = Resources.Load<Dialogue>("Dialogues/Tutorial");
-        dialogueUI.StartDialogue(dialogue);
+        cutsceneDialogueManager.StartDialogue(dialogue);
     }
 }
