9e70c7f9d909b609b4b694a43dfea35a378f02c6
batovski <56854166+batovski@users.noreply.github.com>
2021-02-20 23:38:11 -0800

    Trade UI fix, Inventory Fix, Goblin Poison Effect added

--- Assets/Scripts/NPC/NPC/NPCenemy/Goblin.cs
+++ Assets/Scripts/NPC/NPC/NPCenemy/Goblin.cs
@@ -10,6 +10,7 @@ public class Goblin : Warrior
     private bool _started = false;
     private Animator animator;
 
+    
     protected override void Start()
     {
         base.Start();
@@ -18,12 +19,20 @@ public class Goblin : Warrior
 
     protected override void Attack()
     {
+        GoblinStat _goblinStats = stats as GoblinStat;
         Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(_attackPosition, attackRadius, _whatIsEnemy);
         for (int i = 0; i < enemiesToDamage.Length; i++)
         {
             if (enemiesToDamage[i] != gameObject)
                 enemiesToDamage[i].GetComponent<IDamaged>().
                         TakeDamage(stats.PhysicalDamage.Value, stats.MagicDamage.Value);
+
+            if (Random.value < _goblinStats.PoisonChance)
+            {
+                CharacterStat character = enemiesToDamage[i].GetComponent<CharacterStat>();
+                if (character)
+                    character.EffectController.AddEffect(Instantiate(_goblinStats.PoisonEffect), character);
+            }
         }
 
         isAttack = false;
--- Assets/Scripts/Stats/GoblinStat.cs
+++ Assets/Scripts/Stats/GoblinStat.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class GoblinStat : EnemyStat
 {
+    public float PoisonChance;
+
+    public Effect PoisonEffect;
+
     public override void SetLevel(int level)
     {
         maxHealth = 6 + 3 * level;
         PhysicalDamage.SETBASE(2 + level * 5);
+        PoisonChance = 0.2f + 0.1f * level;
     }
 }
