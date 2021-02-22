6e93c88533b14391670586a373bb1473d878e373
Vladimir Mishakov <38110241+telf01@users.noreply.github.com>
2021-02-23 01:32:13 +0300

    Add turret base.

---
+++ Assets/Scripts/Tutorial/Turret.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private float cooldown;
+    private float _cooldown;
+
+    [SerializeField] private GameObject projectile;
+    [SerializeField]private Animator animator;
+    private static readonly int IsCharging = Animator.StringToHash("IsCharging");
+
+    private void Awake()
+    {
+        _cooldown = cooldown;
+    }
+
+    private void Update()
+    {
+        if (_cooldown >= 0)
+        {
+            _cooldown -= Time.deltaTime;
+            animator.SetBool(IsCharging, false);
+        }
+        else
+        {
+            animator.SetBool(IsCharging, true);
+            Fire();
+            _cooldown = cooldown;
+        }
+    }
+
+    private void Fire()
+    {
+        Instantiate(projectile, transform.position, Quaternion.Euler(transform.rotation.x, transform.rotation.y, transform.rotation.z - 90f));
+    }
+}
---
+++ Assets/Scripts/Tutorial/TurretProjectile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class TurretProjectile : MonoBehaviour
+{
+    private int wallLayer;
+    private int playerLayer;
+
+    [SerializeField] private float speed = 1f;
+    
+    public float Damage { get; set; }
+
+    private Vector3 direction;
+    
+    private void Awake()
+    {
+        wallLayer = LayerMask.NameToLayer("Wall");
+        playerLayer = LayerMask.NameToLayer("Player");
+        direction = new Vector3();
+    }
+
+    private void Update()
+    {
+        transform.position += transform.right * (speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        if (other.gameObject.layer == wallLayer)
+        {
+            Destroy(gameObject);
+        }
+        else if (other.gameObject.layer == playerLayer)
+        {
+            DealDamage();
+            Destroy(gameObject);
+        }
+    }
+
+    private void DealDamage()
+    {
+        PlayerOnScene.Instance.stats.CurrentHealth -= Damage;
+    }
+}
