8ac94d19d8ddae5405fbe14753bccd17626d7406
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2021-06-20 17:00:07 +0300

    Test AI for mini game

---
+++ Assets/Scripts/MiniGames/MiniGameUI.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class MiniGameUI : MonoBehaviour
+{
+    public GameObject miniGameWindow;
+    public Image[] fieldImages;
+    public Sprite[] coinSprites;
+    public TextMeshProUGUI amountUI;
+    public TextMeshProUGUI turnUI;
+    public Button buttonExit;
+    public Button buttonThrow;
+
+    public void OpenMiniGame()
+    {
+        InterfaceManager.Instance.HideFaceElements();
+        InterfaceManager.Instance.HideTavernUI();
+        miniGameWindow.gameObject.SetActive(true);
+    }
+
+    public void CloseMiniGame()
+    {
+        miniGameWindow.gameObject.SetActive(false);
+        InterfaceManager.Instance.ShowFaceElements();
+        InterfaceManager.Instance.ShowTavernUI();
+
+    }
+
+    public void Defeat()
+    {
+        CloseMiniGame();
+         // swhows lose window
+    }
+
+    public void Victory()
+    {
+        CloseMiniGame();
+         // shows victory window
+    }
+}
