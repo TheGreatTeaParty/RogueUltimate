b9d2c575f13c6b0ac5cc1f7af3c9f932c8379ba4
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2021-06-30 17:23:48 +0300

    Added jerk skill
     - small changes
     - added jerk

--- Assets/Scripts/Abilities/Dash.cs
+++ Assets/Scripts/Abilities/Dash.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 
 [CreateAssetMenu(menuName = "Abilities/Dash")] 
@@ -9,10 +10,32 @@ public class Dash : ActiveAbility
     
     public override void Activate()
     {
+        base.Activate();
+            
         var player = PlayerOnScene.Instance;
+        var trail = player.playerMovement.trailRenderer;
 
         if (player.playerAttack.CurrentAttackCD <= 0)
+        {
             player.rb.AddForce(player.playerMovement.GetDirection() * bounce);
+            Instantiate(trail, player.GetComponent<Transform>());
+
+            _enemyMask = LayerMask.GetMask("Enemy");
+            Collider2D[] enemiesToDamage = Physics2D.
+                OverlapCircleAll(player.gameObject.transform.position, 2, _enemyMask);
+
+            for (int i = 0; i < enemiesToDamage.Length; i++)
+            {
+                var vector = enemiesToDamage[i].gameObject.transform.position - player.transform.position;
+                enemiesToDamage[i].GetComponent<EnemyStat>().
+                    TakeDamage(3, 0f, vector.normalized, 100000);
+            }
+        }
+/*
+        collider.enabled = false;
+        trail.gameObject.SetActive(false);*/
+            
     }
+        
     
 }
