e639004f615ef8eb09953be8d1a9c69541dc7aa2
Lev Sokolov <Lev.Sokolov.01@mail.ru>
2021-08-23 15:55:35 +0300

    Poisoned stab skill added
     - added anim. for skills
     - added 3rd robery skill
     - some small changes in scripts

---
+++ Assets/Scripts/Abilities/PoisonedStabAbility.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilities/Poisoned stab")]
+public class PoisonedStabAbility : ActiveAbility
+{
+    [SerializeField]
+    private Effect _effect;
+    [SerializeField]
+    private Effect _poisonEffect;
+
+    public override void Activate()
+    {
+        base.Activate();
+
+        var player = PlayerOnScene.Instance;
+
+        player.stats.EffectController.
+            AddEffect(Instantiate(_effect), PlayerOnScene.Instance.stats);
+
+        _enemyMask = LayerMask.GetMask("Enemy");
+        Collider2D enemyToDamage = Physics2D.
+            OverlapCircle(player.gameObject.transform.position, 1, _enemyMask);
+
+        enemyToDamage.GetComponent<CharacterStat>().EffectController.
+            AddEffect(Instantiate(_poisonEffect), enemyToDamage.GetComponent<CharacterStat>());
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
--- Assets/Scripts/Abilities/RepulsiveFeintAbility.cs
+++ Assets/Scripts/Abilities/RepulsiveFeintAbility.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Abilities/Repulsive feint")]
 public class RepulsiveFeintAbility : ActiveAbility
 {
-
+    [SerializeField]
+    private Effect _effect;
 
     protected override void Update()
     {
@@ -23,13 +24,15 @@ public class RepulsiveFeintAbility : ActiveAbility
         Collider2D[] enemiesToDamage = Physics2D.
             OverlapCircleAll(player.gameObject.transform.position, 2, _enemyMask);
 
+        PlayerOnScene.Instance.stats.EffectController.
+            AddEffect(Instantiate(_effect), PlayerOnScene.Instance.stats);
 
         // Damage near enamies
         for (int i = 0; i < enemiesToDamage.Length; i++)
         {
             var vector = enemiesToDamage[i].gameObject.transform.position - player.transform.position;
             enemiesToDamage[i].GetComponent<EnemyStat>().
-                TakeDamage(0f, 0f, player.playerMovement.GetDirection(), 100000);
+                TakeDamage(0f, 0f, player.playerMovement.GetDirection(), 750000);
         }
     }
 }
